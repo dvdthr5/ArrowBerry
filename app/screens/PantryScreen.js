e73450b71d063763525d9dfa73906e1d45a8208d
PurrGod <srinid369@gmail.com>
2026-05-13 14:10:56 -0700

    Pass the final test for pantry screen.

--- app/screens/PantryScreen.js
+++ app/screens/PantryScreen.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useState } from 'react';
 import {
   ActivityIndicator,
   Alert,
@@ -84,7 +85,11 @@ export default function PantryScreen() {
     setRefreshing(false);
   };
 
-  useEffect(() => { fetchItems(); }, []);
+  useFocusEffect(
+    useCallback(() => {
+      fetchItems();
+    }, [])
+  );
 
   const onRefresh = () => { setRefreshing(true); fetchItems(); };
 
